a9e7e6b7f52a0147380a3a267e2e7bebb492853e
Ivan Rodkin <irodkin2002@gmail.com>
2025-03-02 18:22:58 +0400

    change grad clip value and predict from mask in bash script

--- scripts/cell_autom/finetune_ca_gptneox.sh
+++ scripts/cell_autom/finetune_ca_gptneox.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export CUDA_VISIBLE_DEVICES=4
+export CUDA_VISIBLE_DEVICES=1
 NP=$(echo $CUDA_VISIBLE_DEVICES | awk -F',' '{print NF}')
 export NCCL_ASYNC_ERROR_HANDLING=0
 set -e
@@ -42,7 +42,7 @@ MODEL_CFG=~/rmt/wip/base_models/gptconfigs/neox_tiny_${NUM_LAYERS}l${NUM_LAYERS}
 
 
 
-for N in 8
+for N in 10
 do
 
 
@@ -110,12 +110,12 @@ accelerate launch --num_processes $NP --config_file  ./accelerate.yaml --main_pr
         --show_valid_examples 5 \
         --early_stopping_patience 30 \
         --seed $(($N+42*$j)) \
-        --clip_grad_value 0.1 \
+        --clip_grad_value 0.5 \
         --save_best \
         --d_mem $D_MEM \
         --layers_attr gpt_neox.layers \
         --freeze_mem \
-        --validate_only
+        --predict_from_mask
         # --act_on \
         # --max_hop $MAX_HOP \
         # --time_penalty 3e-4 \
