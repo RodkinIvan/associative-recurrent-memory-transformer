7d5564089de0c982fb9ad44dd49a1e5e0da8b9e2
Ivan Rodkin <Ivan.Rodkin@mbzuai.ac.ae>
2025-03-30 12:48:00 +0400

    fix typo

--- scripts/pg19/tokenize_pg19.py
+++ scripts/pg19/tokenize_pg19.py
@@ -3,7 +3,7 @@ import numpy as np
 from transformers import AutoModel, AutoTokenizer
 
 dataset = load_dataset('pg19', trust_remote_code=True)
-model_path = "/home/jovyan/kuratov/models/Llama-3.2-1B"
+model_path = "meta-llama/Llama-3.2-1B"
 tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 
 def tokenize(sample):
@@ -11,4 +11,4 @@ def tokenize(sample):
     return sample
 
 new_ds = dataset.map(tokenize, batch_size=256)
-new_ds.save_to_disk('/mnt/data/uesrs/ivan.rodkin/lab/datasets/pg19_tokenized')
+new_ds.save_to_disk('/mnt/data/users/ivan.rodkin/lab/datasets/pg19_tokenized')
