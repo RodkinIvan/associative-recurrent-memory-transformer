9edd6cd0319db8786b038bfd13bba6eab60af294
Ivan Rodkin <Ivan.Rodkin@mbzuai.ac.ae>
2025-03-30 22:29:18 +0400

    make 3rd zero stage in deepspeed

--- accel_configs/create_config.py
+++ accel_configs/create_config.py
@@ -35,7 +35,7 @@ deepspeed_config = {
         "enabled": "auto"
     },
     "zero_optimization": {
-        "stage": 2
+        "stage": 3
     },
     "gradient_accumulation_steps": None,
     "gradient_clipping": 1.0,
